87c3335ff0ff5a02d4b98f44e0dd1cca28d09217
Vigopi <vinothgopi97@gmail.com>
2018-03-11 08:04:25 +0530

    next commit

--- db/schema.rb
+++ db/schema.rb
@@ -63,14 +63,4 @@ ActiveRecord::Schema.define(version: 20171216045116) do
     t.datetime "updated_at",             null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "email",      limit: 255
-    t.string   "mobile",     limit: 255
-    t.string   "username",   limit: 255
-    t.string   "password",   limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
-
 end
